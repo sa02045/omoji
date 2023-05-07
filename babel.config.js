0d96ea5aba05040ed2638b2c42713ed7bf324a3f
sa02045 <sa02045@naver.com>
2023-05-07 14:36:53 +0900

    chore: alias 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
