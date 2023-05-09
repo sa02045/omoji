c9c114d72bba4a62bf3b0db33dfc7373b83f754c
sa02045 <sa02045@naver.com>
2023-05-09 21:05:21 +0900

    feat: Uplaod screen

---
+++ src/screens/UploadScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+export function UploadScreen() {
+  return (
+    <View style={styles.container}>
+      <View />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#17171B',
+    flex: 1,
+  },
+});
