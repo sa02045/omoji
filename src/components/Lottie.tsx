3ee7b50bfe940ecc602c0cc378dc7ad22d725158
sa02045 <sa02045@naver.com>
2023-05-09 21:00:20 +0900

    feat: Lottie 애니메이션

---
+++ src/components/Lottie.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {View} from 'react-native';
+import {StyleSheet} from 'react-native';
+
+import LottieView from 'lottie-react-native';
+
+export function Lottie({type}: {type: 'good' | 'bad'}) {
+  return (
+    <View style={styles.lottieContainer}>
+      {type === 'good' ? (
+        <LottieView
+          source={require('../assets/lottie/good.json')}
+          style={styles.lottie}
+          autoPlay
+          speed={1.5}
+          loop={false}
+        />
+      ) : (
+        <LottieView
+          source={require('../assets/lottie/bad.json')}
+          style={styles.lottie}
+          autoPlay
+          speed={1.5}
+          loop={false}
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  lottieContainer: {
+    position: 'absolute',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  lottie: {
+    width: 100,
+    height: 100,
+  },
+});
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,13 +1,24 @@
 import {Dimensions, StyleSheet, View} from 'react-native';
-import React from 'react';
 import {EvaluateButton} from '../components/EvaluateButton';
+import {useState} from 'react';
+import React from 'react';
+import {Lottie} from '../components/Lottie';
+
 export function MainScreen() {
+  const [lottieType, setLottieType] = useState<'good' | 'bad' | null>(null);
+  const onPressLottie = (type: 'good' | 'bad') => () => {
+    setLottieType(type);
+    setTimeout(() => {
+      setLottieType(null);
+    }, 1000);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        <EvaluateButton type="hmm" onPress={() => {}} />
-        <EvaluateButton type="good" onPress={() => {}} />
+        <EvaluateButton type="hmm" onPress={onPressLottie('bad')} />
+        <EvaluateButton type="good" onPress={onPressLottie('good')} />
       </View>
+      {lottieType && <Lottie type={lottieType} />}
     </View>
   );
 }
@@ -25,5 +36,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     left: width / 2 - width / 4,
     bottom: 30,
+  },
+  lottieWrapper: {
+    position: 'absolute',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
   },
 });
