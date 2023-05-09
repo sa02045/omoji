24256b8104bdcacf775767198475bb8c9d7200df
sa02045 <sa02045@naver.com>
2023-05-09 21:55:32 +0900

    feat: Preview screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import {Alert, Image, Pressable, StyleSheet, Text, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
+import NaverLogin from '@react-native-seoul/naver-login';
+import Config from 'react-native-config';
+import {requestGetNaverLogin} from '../api/auth';
+import {loginAtom} from '../atoms/LoginAtom';
+import {RecoilState, useRecoilState} from 'recoil';
 
 type StackParamList = {
   Login: undefined;
@@ -9,7 +14,40 @@ type StackParamList = {
 };
 
 export function LoginScreen() {
+  const consumerKey = Config.NAVER_APP_KEY;
+  const consumerSecret = Config.NAVER_APP_SECRET_KEY;
+  const appName = Config.APP_NAME;
+  const serviceUrlScheme = Config.SERVICE_URL_SCHEME;
+
+  const [_, setLogin] = useRecoilState(loginAtom);
+
   const {navigate} = useNavigation<StackNavigationProp<StackParamList>>();
+
+  const onPressNaverLogin = async () => {
+    try {
+      await fetchNaverLogin();
+    } catch (e) {
+      Alert.alert('로그인 실패');
+    }
+  };
+
+  const fetchNaverLogin = async () => {
+    const {failureResponse, successResponse} = await NaverLogin.login({
+      appName,
+      consumerKey,
+      consumerSecret,
+      serviceUrlScheme,
+    });
+    if (successResponse) {
+      await requestGetNaverLogin(successResponse.accessToken);
+      setLogin(true);
+      return;
+    }
+
+    if (failureResponse) {
+      Alert.alert('로그인 실패');
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -34,7 +72,7 @@ export function LoginScreen() {
         <Pressable style={styles.loginContainer}>
           <Text style={styles.loginText}>로그아웃</Text>
         </Pressable>
-        <Pressable style={styles.loginContainer}>
+        <Pressable style={styles.loginContainer} onPress={onPressNaverLogin}>
           <Text style={styles.loginText}>네이버로 시작하기</Text>
         </Pressable>
         <Pressable>
--- src/screens/PreviewScreen.tsx
+++ src/screens/PreviewScreen.tsx
@@ -1,16 +1,22 @@
 import React, {useState} from 'react';
-import {Image, View} from 'react-native';
-import {Dimensions, TouchableOpacity} from 'react-native';
-
+import {Image, View, StyleSheet} from 'react-native';
+import {TouchableOpacity, Dimensions} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import Preview1 from '../assets/preview1.png';
 import Preview2 from '../assets/preview2.png';
 import Preview3 from '../assets/preview3.png';
+import {StackNavigationProp} from '@react-navigation/stack';
+type StackParamList = {
+  Login: undefined;
+};
 
 const {width} = Dimensions.get('window');
 
 export const PreviewScreen = () => {
   const [preview, setPreview] = useState(Preview1);
   const [step, setStep] = useState(0);
+
+  const {navigate} = useNavigation<StackNavigationProp<StackParamList>>();
 
   return (
     <View>
@@ -23,10 +29,19 @@ export const PreviewScreen = () => {
           } else if (step === 1) {
             setPreview(Preview3);
             setStep(2);
+          } else {
+            navigate('Login');
           }
         }}>
-        <Image source={preview} style={{height: '100%', width}} />
+        <Image source={preview} style={styles.image} />
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    height: '100%',
+    width,
+  },
+});
