72056f98ef68134e1c1ec18c368faf6743ac6e9c
sa02045 <sa02045@naver.com>
2023-05-09 19:36:45 +0900

    feat: 버튼 위치

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,14 +1,29 @@
-import {View} from 'react-native';
-
+import {Dimensions, StyleSheet, View} from 'react-native';
 import React from 'react';
 import {EvaluateButton} from '../components/EvaluateButton';
 export function MainScreen() {
   return (
-    <View>
-      <View>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
         <EvaluateButton type="good" onPress={() => {}} />
         <EvaluateButton type="hmm" onPress={() => {}} />
       </View>
     </View>
   );
 }
+
+const {width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'row',
+    left: width / 2 - 100,
+    bottom: 30,
+  },
+});
