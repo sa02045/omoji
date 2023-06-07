24332607f0822986c9641649200fca8eaffbb15a
sa02045 <sa02045@naver.com>
2023-06-07 10:53:04 +0900

    feat: 로그인 버튼

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -58,6 +58,40 @@ export function LoginScreen() {
     }
   }
 
+  async function onPressNaverLogin() {
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
+    });
+    const credentialState = await appleAuth.getCredentialStateForUser(
+      appleAuthRequestResponse.user,
+    );
+
+    if (credentialState === appleAuth.State.AUTHORIZED) {
+      const {identityToken} = appleAuthRequestResponse;
+      if (!identityToken) {
+        Alert.alert('로그인 실패', '로그인에 실패했습니다.');
+        return;
+      }
+
+      try {
+        const {accessToken, refreshToken, isNewUser, nickname} =
+          await requestPostAppleLogin(identityToken);
+
+        await EncryptedStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+        await EncryptedStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
+
+        if (isNewUser) {
+          navigate('NickName');
+        } else {
+          await EncryptedStorage.setItem(NICKNAME_KEY, nickname);
+          setLogin(true);
+        }
+      } catch (e) {
+        Alert.alert('로그인 실패', JSON.stringify(e));
+      }
+    }
+  }
   return (
     <View style={styles.container}>
       <Pressable
@@ -75,12 +109,63 @@ export function LoginScreen() {
         />
       </View>
       <View style={styles.loginButtonContainer}>
-        <AppleButton
-          buttonStyle={AppleButton.Style.DEFAULT}
-          buttonType={AppleButton.Type.SIGN_IN}
-          style={styles.appleLogin}
-          onPress={() => onPressAppleLogin()}
-        />
+        <Pressable
+          style={{
+            flexDirection: 'row',
+            backgroundColor: '#fff',
+            height: 56,
+            borderRadius: 10,
+            alignSelf: 'flex-end',
+            marginBottom: 12,
+          }}>
+          <View
+            style={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              padding: 20,
+            }}>
+            <Image
+              source={require('../assets/Apple.png')}
+              style={{width: 36, height: 36}}
+            />
+            <Text
+              style={{
+                fontWeight: 'bold',
+                textAlign: 'center',
+                marginLeft: 56,
+              }}
+              onPress={onPressAppleLogin}>
+              Apple로 시작하기
+            </Text>
+          </View>
+        </Pressable>
+        <Pressable style={styles.loginContainer}>
+          <View
+            style={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              padding: 20,
+            }}>
+            <Image
+              source={require('../assets/Naver.png')}
+              style={{width: 36, height: 36}}
+            />
+            <Text
+              style={{
+                fontWeight: 'bold',
+                color: '#fff',
+                textAlign: 'center',
+                marginLeft: 56,
+              }}
+              onPress={onPressNaverLogin}>
+              네이버로 시작하기
+            </Text>
+          </View>
+        </Pressable>
       </View>
     </View>
   );
@@ -130,5 +215,19 @@ const styles = StyleSheet.create({
   },
   loginButtonContainer: {
     marginTop: 50,
+  },
+
+  loginContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#03C75A',
+    height: 56,
+    borderRadius: 10,
+    alignSelf: 'flex-end',
+  },
+  loginText: {
+    flex: 1,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    alignSelf: 'center',
   },
 });
