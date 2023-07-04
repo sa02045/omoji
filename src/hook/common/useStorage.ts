e9c971a085b4d122714810317237d8c4107f2bd0
sa02045 <sa02045@naver.com>
2023-07-04 21:16:10 +0900

    feat: 사용중인 스토리지 key 목록을 타입으로

--- src/hook/common/useStorage.ts
+++ src/hook/common/useStorage.ts
@@ -1,11 +1,17 @@
 import storage from '../../utils/Storage';
 
+const STORAGE_KEY = {
+  ACCESS_TOKEN_KEY: 'ACCESS_TOKEN_KEY',
+  REFRESH_TOKEN_KEY: 'REFRESH_TOKEN_KEY',
+  NICKNAME_KEY: 'NICKNAME_KEY',
+} as const;
+
 export function useStorage() {
   async function setStorage(key: string, value: unknown) {
     await storage.setItem(key, value);
   }
 
-  async function getStorage(key: string) {
+  async function getStorage(key: keyof typeof STORAGE_KEY) {
     const value = await storage.getItem(key);
     return value;
   }
