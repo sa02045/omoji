54a60aa75b5e61dda0cd716809ff167dfe0bf4fe
sa02045 <sa02045@naver.com>
2023-07-05 22:54:37 +0900

    del unimported code

--- src/hook/services/mutations/useEvaluate.ts
+++ src/hook/services/mutations/useEvaluate.ts
@@ -1,6 +1,5 @@
 import {useQuery} from '@tanstack/react-query';
 import {fetchEvaluate} from '../../../api/evaluate';
-import {UseQueryOptions} from '@tanstack/react-query';
 
 export function useEvaluate() {
   return useQuery({
